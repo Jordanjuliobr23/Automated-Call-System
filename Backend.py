81e752256e52a69d5292f43b6750145f6823508c
Jordan Júlio <164351071+Jordanjuliobr23@users.noreply.github.com>
2025-11-04 21:51:54 -0300

    Update Backend.py

--- Backend.py
+++ Backend.py
@@ -1,8 +1,6 @@
-# Bibliotecas 
-# ...existing code...
+
 from fastapi import FastAPI, HTTPException, Request 
 from fastapi.responses import RedirectResponse, StreamingResponse 
-# ...existing code...
 import uuid, io, time
 from datetime import datetime, timedelta
 import qrcode
@@ -12,6 +10,7 @@ TOKENS = {}
 
 TIME= 180 
 LOGIN = "https://suap.ifrn.edu.br/accounts/login/?next=/api/"
+
 
 
 
