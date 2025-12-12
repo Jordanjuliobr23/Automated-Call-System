2f67e24679b7be7fb7827ef7198ec5be11976799
Ian Guilherme <ianguilherme19@hotmail.com>
2025-12-12 02:34:21 -0300

    Atualização na geração de qr codes
    Feito a integração do django com a geração de qr codes
    Closed #23
    Closed #27

--- Codigos/frontend/chamadas/urls.py
+++ Codigos/frontend/chamadas/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
 from .views import registrar_aluno, ProfessorCreate
 from .views import DisciplinaList, DiarioList, AulaList, AlunoList, ChamadaList
+from .views import (gerar_chave,mostrar_qr,expirar_chave,registrar_aluno)
 
 urlpatterns = [
     #path("", IndexView.as_view(), name="index"),
-    path("aluno/", registrar_aluno, name="aluno"),
     path("professor/", ProfessorCreate.as_view(), name="professor"),
     path("professor/listar/disciplina", DisciplinaList.as_view(), name="listar-disciplina"),
     path("professor/listar/diario", DiarioList.as_view(), name="listar-diario"),
     path("professor/listar/aula", AulaList.as_view(), name="listar-aula"),
     path("professor/listar/aluno", AlunoList.as_view(), name="listar-aluno"),
     path("professor/listar/chamada", ChamadaList.as_view(), name="listar-chamada"),
+    
+    path("gerar/<int:aula_id>/", gerar_chave, name="gerar_chave"),
+    path("qr/<str:codigo>/", mostrar_qr, name="mostrar_qr"),
+    path("chave/<str:codigo>/expirar/", expirar_chave, name="expirar_chave"),
+    path("aluno/<str:codigo>/", registrar_aluno, name="registrar_aluno"),
 ]
